a99ad99602b4dc599f48ea9025fac8ec14ccfd1c
Mobolaji02 <makinwamobolaji@gmail.com>
2025-09-16 23:33:29 +0100

    feat: add login page

--- X-Clone/src/App.jsx
+++ X-Clone/src/App.jsx
@@ -1,10 +1,19 @@
+import React from "react"
 import Home from "./components/Home"
+import Login from "./components/Login"
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import LoginPage from "./Pages/LoginPage"
 function App() {
+const router= createBrowserRouter(createRoutesFromElements(
+  <Route index element={<LoginPage/>}/>
 
+))
 
   return (
     <>
-     <Home /> 
+     {/* <Home />  */}
+     <Login/>
+     
     </>
   )
 }
