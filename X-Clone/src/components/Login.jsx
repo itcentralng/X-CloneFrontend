383224c0c4cf7d34b372ae744c0aec9da247c4a6
Mobolaji02 <makinwamobolaji@gmail.com>
2025-09-24 12:54:18 +0100

    Added the sign-in page

--- X-Clone/src/components/Login.jsx
+++ X-Clone/src/components/Login.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Login.css'
 const Login = () => {
+  const [showModal, setShowModal] = React.useState(false)
+  function handleShowModal(){
+    setShowModal(prevShowModal=>!prevShowModal)
+  }
   return (
     <>
       <div className="container">
-        <div className="login-container">
+        
+        <div className=  {`login-container ${showModal ? "overlay" : null}`}  >
           <div className="x-logo">
             <img src="/src/images/Ninja-x.png" alt="X-image" />
           </div>
@@ -28,14 +33,57 @@ const Login = () => {
             <p className='low-description'>By signing up, you agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>, including <a href="">Cookie use.</a></p>
 
             <h2 className='Already-heading'>Already have an account?</h2>
-            <button className='social-button font-bold bg-dark'>Sign in</button>
+            <button onClick={handleShowModal} className='social-button font-bold bg-dark'>Sign in</button>
             <button className='social-button font-bold bg-dark'>
               Get Grok
             </button>
-          </div>      
+          </div>  
+
+             {
+
+          showModal &&
+        <div className="signIn-container">
+          <div className="x-logo">
+            <img src="/src/images/Ninja-x.png" alt="X-image" />
+          </div>
+
+          <div className="social-login">
+            <h2 className="sign-heading">Sign in to X</h2>
+
+            <button type="button" className="social-button">
+              <img src="/src/images/google-logo.png" alt="google-logo" />
+              Sign up with Google
+            </button>
+
+            <button type="button" className="social-button">
+               <img src="/src/images/apple-logo.png" alt="apple-logo" />
+              Sign up with Apple
+            </button>
+
+            <p className="separator"><span>or</span></p>
+
+            <input 
+              type="text" 
+              placeholder="Phone, email, or username" 
+              className="input-field"
+            />
+
+            <button type="button" className="social-button font-bold">Next</button>
+            <button type="button" className="social-button font-bold bg-dark">
+              Forgot password?
+            </button>
+
+            <p className="low-description">
+              By signing up, you agree to the 
+              <a href="#"> Terms of Service </a>
+            </p>
+          </div>
+        </div>
+        }  
         </div>
         
 
+       
 
       </div> 
     </>
