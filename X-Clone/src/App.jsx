607be57cdd05fe194f0ee4944658ccb1095d51bd
Amir Abdullahi <amirabdullahi011@gmail.com>
2025-09-26 10:14:39 +0100

    Clean up node_modules and update gitignore

--- X-Clone/src/App.jsx
+++ X-Clone/src/App.jsx
@@ -1,21 +1,26 @@
-import React from "react"
-import Home from "./components/Home"
-import SignUpPage from "./Pages/SignUpPage"
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes} from 'react-router-dom'
-import LoginPage from "./Pages/LoginPage"
+import React from "react";
+import Home from "./components/Home";
+import SignUpPage from "./Pages/SignUpPage";
+import { Route, createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
 
-  function App() {
-    const router= createBrowserRouter(createRoutesFromElements(
-     
-  <>
-  <Route index element={<LoginPage/>}/>
-  <Route path="/signup" element={<SignUpPage />} />
-  <Route path="/home" element={<Home />} />
- </>
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage />,
+    },
+    {
+      path: "/signup",
+      element: <SignUpPage />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  ]);
 
-));
   return <RouterProvider router={router} />;
-  
 }
 
-export default App
+export default App;
