df5b273b11ac9227a363e4f0360d9a5a742a7f8c
USER <aishamuarin@gmail.com>
2025-09-26 11:20:02 +0100

    route path add to App.jsx

--- X-Clone/src/App.jsx
+++ X-Clone/src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 import Home from "./components/Home"
 import SignUpPage from "./Pages/SignUpPage"
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes} from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import LoginPage from "./Pages/LoginPage"
 
   function App() {
     const router= createBrowserRouter(createRoutesFromElements(
      
-  <>
+  <Route path="/">
   <Route index element={<LoginPage/>}/>
   <Route path="/signup" element={<SignUpPage />} />
   <Route path="/home" element={<Home />} />
- </>
+ </Route>
 
 ));
   return <RouterProvider router={router} />;
